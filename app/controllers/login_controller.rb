9ba6717677905950399479ca42b90aa091105c14
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-12 17:18:36 +1200

    @user is now set to the current logged in user

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,15 @@
 enable :sessions
 
+before do
+  @user = User.find_by_id(session[:user_id])
+end
+
 post '/login' do
   user = User.find_by_username(params[:username])
 
   if user.check_password(params[:password])
     session_login(user.id)
-    redirect to '/profile'
+    redirect to request.referrer
   else
     redirect to '/'
   end
